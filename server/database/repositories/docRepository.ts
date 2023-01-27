7df0266758658b6410a6a3120abd8d69f321f807
preginald <github.campout@simplelogin.com>
2023-01-27 13:05:39 +1100

    Include author model

--- server/database/repositories/docRepository.ts
+++ server/database/repositories/docRepository.ts
@@ -1,5 +1,22 @@
 import prisma from "../client";
 
+export async function createDoc(data: any) {
+  const authorId = "7c96e0b5-9dd9-11ed-b676-0242ac110011";
+  const title = data.title;
+  const doc = await prisma.doc.create({
+    data: {
+      title,
+      author: {
+        connect: {
+          id: authorId,
+        },
+      },
+    },
+  });
+  return doc;
+}
 export async function getDocs() {
-  return await prisma.doc.findMany();
+  return await prisma.doc.findMany({
+    include: { author: true },
+  });
 }
