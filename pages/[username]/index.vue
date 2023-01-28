856883f57b67689c464a4dd32ba1740b3b692978
preginald <github.campout@simplelogin.com>
2023-01-28 19:03:31 +1100

    Get Docs related to username

---
+++ pages/[username]/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtLink v-for="doc in docs" :to="'/preginald/' + doc.slug">
+      <div x class="card-container">
+        {{ doc.title }}
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const username: string | string[] = useRoute().params.username;
+try {
+  const { data: docs } = await useFetch("/api/doc/" + username);
+} catch (error) {
+  error;
+}
+</script>
+k
