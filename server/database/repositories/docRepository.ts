905e421107f77dfd33ab0e34078dcf623c9ed725
preginald <github.campout@simplelogin.com>
2023-01-28 14:46:15 +1100

    Get user docs and doc by id

--- server/database/repositories/docRepository.ts
+++ server/database/repositories/docRepository.ts
@@ -1,23 +1,5 @@
 import prisma from "../client";
 
-// export async function createDoc(data: any) {
-//   console.log(data);
-
-//   const authorId = "7c96e0b5-9dd9-11ed-b676-0242ac110011";
-//   const title = data.title;
-//   const doc = await prisma.doc.create({
-//     data: {
-//       title,
-//       author: {
-//         connect: {
-//           id: authorId,
-//         },
-//       },
-//     },
-//   });
-//   return doc;
-// }
-
 export async function createDoc(data: any) {
   console.log(data);
 
@@ -55,43 +37,6 @@ export async function createDoc(data: any) {
   return doc;
 }
 
-// export async function createDoc(data: any) {
-//   console.log(data);
-//   const title = data.title;
-//   const authorId = data.authorId;
-//   const steps = data.steps;
-
-//   // const doc = await prisma.doc.create({
-//   //   data: {
-//   //     title,
-//   //     author: {
-//   //       connect: {
-//   //         id: authorId,
-//   //       },
-//   //     },
-//   //     steps: {
-//   //       create: steps.map((step: any) => {
-//   //         return {
-//   //           title: step.title,
-//   //           order: step.order,
-//   //           tasks: {
-//   //             create: step.tasks.map((task: any) => {
-//   //               return {
-//   //                 intro: task.intro,
-//   //                 input: task.input,
-//   //                 output: task.output,
-//   //               };
-//   //             }),
-//   //           },
-//   //         };
-//   //       }),
-//   //     },
-//   //   },
-//   // });
-
-//   return doc;
-// }
-
 export async function getDocs() {
   return await prisma.doc.findMany({
     include: {
@@ -103,4 +48,30 @@ export async function getDocs() {
       },
     },
   });
+}
+
+export async function getDocById(id: string) {
+  return await prisma.doc.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      author: true,
+      steps: {
+        include: {
+          tasks: true,
+        },
+      },
+    },
+  });
+}
+
+export async function getUserDocs(username: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+    include: { docs: true },
+  });
+  return user ? user.docs : null;
 }
