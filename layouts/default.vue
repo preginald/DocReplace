971dd5f4200e2ca9bb08f9d3034500df95d9147d
preginald <github.campout@simplelogin.com>
2023-01-28 15:18:46 +1100

    Create a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <ul class="flex gap-4">
+          <li v-for="element in navElements">
+            <NuxtLink :to="element.route">{{ element.name }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+
+  <div class="container mx-auto p-4">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const navElements = [
+  { route: "/", name: "Home" },
+  { route: "/preginald", name: "My Docs" },
+  { route: "/docs/create", name: "New" },
+];
+</script>
+
+<style scoped>
+.router-link-exact-active {
+  color: red;
+}
+</style>
