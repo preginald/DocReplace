70605d801d67b4b92aac74586ac234da92c478a7
preginald <github.campout@simplelogin.com>
2023-01-28 19:03:50 +1100

    Remove comments

--- server/api/doc/index.get.ts
+++ server/api/doc/index.get.ts
@@ -5,12 +5,5 @@ export default defineEventHandler(async (event) => {
 
   const docs = await getDocs();
 
-  // const docs = [
-  //   {
-  //     id: "2039i0s9fi09",
-  //     user_id: "preginald",
-  //     title: "How to use Rsync",
-  //   },
-  // ];
   return docs;
 });
