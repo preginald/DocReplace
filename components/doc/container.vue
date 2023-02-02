6459a46950b89c03551a6c9aeedf34da32e26cc9
preginald <github.campout@simplelogin.com>
2023-02-03 08:33:28 +1100

    Move tasks block to a component

--- components/doc/container.vue
+++ components/doc/container.vue
@@ -25,27 +25,7 @@
       <h2>Step {{ step.order }}: {{ step.title }}</h2>
       <hr />
       <div v-for="task in step.tasks">
-        <p class="mb-5">
-          {{ task.intro }}
-        </p>
-        <div class="py-3">
-          <span class="badge-input">
-            <span>Input</span>
-            <span v-if="task.language.name"> / {{ task.language.name }}</span>
-          </span>
-          <div class="input-container">
-            <pre class="p-3">
-<span v-if="task.language.name">{{ task.language.prompt }}</span> {{ replacePlaceholders(doc.inputs, task.input) }}</pre>
-          </div>
-        </div>
-        <div v-if="task.output" class="py-3">
-          <span class="badge-output">Output</span>
-          <div class="output-container">
-            <pre
-              class="p-3"
-            ><span v-if="task.language.name">{{ task.language.prompt }}</span> {{ task.output }}</pre>
-          </div>
-        </div>
+        <DocTaskCard :doc="doc" :task="task" />
       </div>
     </div>
   </div>
@@ -53,14 +33,6 @@
 
 <script setup lang="ts">
 const { doc } = defineProps(["doc"]);
-
-const replacePlaceholders = (inputs: any, string: string) => {
-  inputs.forEach((input) => {
-    const regex = new RegExp(`<vv>${input.name}</vv>`, "g");
-    string = string.replace(regex, input.value);
-  });
-  return string;
-};
 </script>
 
 <style>
