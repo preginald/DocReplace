d331798ecea117aa8fdb37842234cbad51f7034f
preginald <github.campout@simplelogin.com>
2023-02-03 14:47:48 +1100

    Redirect user if authenticated

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,41 @@
 
 <script setup lang="ts">
 const { data: doc } = await useFetch("/api/doc");
+
+let user = ref({
+  id: "",
+  email: "",
+  username: "",
+  image: "",
+});
+
+const {
+  status,
+  data,
+  lastRefreshedAt,
+  getCsrfToken,
+  getProviders,
+  getSession,
+  signIn,
+  signOut,
+} = useSession();
+
+const processUser = async (data) => {
+  await $fetch("/api/user/upsert/", {
+    method: "POST",
+    body: data,
+  }).then(async (response) => {
+    user.value.id = response.id;
+    user.value.email = response.email;
+    user.value.username = response.username;
+    user.value.image = response.image;
+    useRouter().push({ path: "/" + user.value.username });
+  });
+};
+
+if (status.value == "authenticated") {
+  processUser(data.value);
+}
 </script>
 
 <style scoped></style>
