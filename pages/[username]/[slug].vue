0e53bbbae461cb91015176ec8e1e1263c4706fd3
preginald <github.campout@simplelogin.com>
2023-02-04 21:28:40 +1100

    Use components

--- pages/[username]/[slug].vue
+++ pages/[username]/[slug].vue
@@ -1,5 +1,7 @@
 <template>
-  <DocContainer :doc="doc" />
+  <DocToolbar :doc="doc" />
+  <DocForm v-if="isDraft() && isInline()" :doc="doc" />
+  <DocContainer v-if="isPreview()" :doc="doc" />
   <!-- <pre>{{ doc }}</pre> -->
 </template>
 
@@ -7,5 +9,21 @@
 const username: string | string[] = useRoute().params.username;
 const slug: string | string[] = useRoute().params.slug;
 
-const { data: doc } = await useFetch("/api/doc/" + username + "/" + slug);
+const { data: doc } = await useFetch(
+  "/api/user/username/" + username + "/doc/" + slug
+);
+
+doc.value.view = "inline";
+
+const isDraft = () => {
+  return doc.value.status == "draft";
+};
+
+const isPreview = () => {
+  return doc.value.view == "preview";
+};
+
+const isInline = () => {
+  return doc.value.view == "inline";
+};
 </script>
