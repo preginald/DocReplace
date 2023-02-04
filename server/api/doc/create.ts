9e82ede00d93459a337704bb18d524949bdd0f96
preginald <github.campout@simplelogin.com>
2023-02-04 21:30:06 +1100

    Remove comments

--- server/api/doc/create.ts
+++ server/api/doc/create.ts
@@ -2,7 +2,6 @@ import { createDoc } from "~/server/database/repositories/docRepository";
 
 export default defineEventHandler(async (event) => {
   const data = await readBody(event);
-  console.log("create.ts:", data);
 
   const newDoc = await createDoc(data);
 
