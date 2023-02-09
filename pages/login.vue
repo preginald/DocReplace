98cc96582e2840cbdcb26af0867a36407f15132d
preginald <github.campout@simplelogin.com>
2023-02-09 11:49:20 +1100

    Create a login page

---
+++ pages/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <p>Sign-In Options:</p>
+    <button @click="signIn('github')" class="btn-default-md">Github</button>
+    <!-- NOTE: Here we hard-coded username and password, on your own page this should probably be connected to two inputs for username + password -->
+    <div class="card-container">
+      <div>
+        <label
+          for="username"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Username</label
+        >
+        <input
+          type="text"
+          id="username"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Username"
+          required
+        />
+      </div>
+      <div>
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Doe"
+          required
+        />
+      </div>
+    </div>
+    <button
+      @click="signIn('credentials', { username, password })"
+      class="btn-default-md"
+    >
+      Sign in
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({ auth: false });
+const { signIn } = useSession();
+const username = ref("");
+const password = ref("");
+</script>
