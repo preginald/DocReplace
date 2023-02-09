ba49f3b1b2e7457db9dec434de77c16d60ce98cd
preginald <github.campout@simplelogin.com>
2023-02-09 11:45:50 +1100

    Get doc by slug and username

---
+++ server/api/user/username/[username]/doc/[slug].get.ts
@@ -0,0 +1,9 @@
+import { getDocBySlugAndUsername } from "~/server/database/repositories/docRepository";
+
+export default defineEventHandler(async (event) => {
+  const { username, slug } = event.context.params;
+
+  const doc = await getDocBySlugAndUsername(slug, username);
+
+  return doc;
+});
