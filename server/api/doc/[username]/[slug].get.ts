8a445c3e7e0c12312f7c3d886a95c13212d3e7eb
preginald <github.campout@simplelogin.com>
2023-02-09 11:42:18 +1100

    Remove buggy code

--- server/api/doc/[username]/[slug].get.ts
+++
@@ -1,8 +0,0 @@
-import { getDocBySlugAndUsername } from "~/server/database/repositories/docRepository";
-
-export default defineEventHandler(async (event) => {
-  const { username, slug } = event.context.params;
-
-  const doc = await getDocBySlugAndUsername(slug, username);
-  return doc;
-});
--- server/api/user/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { getUsers } from "~/server/database/repositories/userRepository";
-
-export default defineEventHandler(async (event) => {
-  const query = getQuery(event);
-
-  const users = await getUsers();
-
-  return users;
-});
